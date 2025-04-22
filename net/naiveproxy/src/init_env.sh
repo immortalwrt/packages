74d80d8243e99a878865083d1e6cfaf0f5230a3c
Tianling Shen <cnsztl@immortalwrt.org>
2025-04-22 23:02:34 +0800

    naiveproxy: Update to 135.0.7049.38-2
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>
    (cherry picked from commit 61fc5c8f8cc77ca22e8196c8850870937532829f)

--- net/naiveproxy/src/init_env.sh
+++ net/naiveproxy/src/init_env.sh
@@ -56,7 +56,6 @@ use_udev=false
 use_aura=false
 use_ozone=false
 use_gio=false
-use_gtk=false
 use_platform_icu_alternatives=true
 use_glib=false
 
@@ -69,6 +68,9 @@ enable_mdns=false
 enable_reporting=false
 include_transport_security_state_preload_list=false
 enable_device_bound_sessions=false
+enable_bracketed_proxy_uris=true
+enable_quic_proxy_support=true
+
 use_nss_certs=false
 
 enable_backup_ref_ptr_support=false
